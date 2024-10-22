62a8918c24579dad9ac46c1145d3d07da7917314
Washington Júnior <wash.j5@gmail.com>
2024-10-22 11:41:26 -0300

    [ADD] MySQL + Spring Docker Compose

--- spring-project/src/main/java/com/uneb/labweb/spring_project/SpringProjectApplication.java
+++ spring-project/src/main/java/com/uneb/labweb/spring_project/SpringProjectApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 
 import com.uneb.labweb.spring_project.enums.Category;
 import com.uneb.labweb.spring_project.model.Course;
@@ -18,6 +19,7 @@ public class SpringProjectApplication {
     }
 
     @Bean
+    @Profile("test")
     CommandLineRunner initDatabase(CourseRepository courseRepository) {
         return args -> {
             courseRepository.deleteAll();
